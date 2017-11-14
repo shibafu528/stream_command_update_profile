9bad1273df6a769c14a8d84d8533b5706611318a
shibafu <shibafu528@gmail.com>
2017-11-14 23:10:41 +0900

    update_prefixコマンドとget_prefixコマンドを追加

--- stream_command_update_profile.rb
+++ stream_command_update_profile.rb
@@ -2,6 +2,48 @@
 
 Plugin.create(:stream_command_update_profile) do
 
+  def update_name(service, message,
+                  prefix: UserConfig[:sc_update_profile_prefix],
+                  suffix: UserConfig[:sc_update_profile_suffix])
+    # changed?
+    prefix_changed = prefix != UserConfig[:sc_update_profile_prefix]
+    suffix_changed = suffix != UserConfig[:sc_update_profile_suffix]
+    
+    # save prefix/suffix
+    UserConfig[:sc_update_profile_prefix] = prefix
+    UserConfig[:sc_update_profile_suffix] = suffix
+
+    # compose new name
+    base_name = UserConfig[:sc_update_profile_base_name]
+    new_name = (prefix + base_name + suffix)[0..49]
+    overflow = (prefix + base_name + suffix) != new_name
+
+    # update profile
+    (service.twitter/'account/update_profile').json(name: new_name).next do
+      msg = "@" + message.user.idname + " "
+      msg += case
+             when prefix_changed && suffix_changed
+               prefix = "(未設定)" if prefix.empty?
+               suffix = "(未設定)" if suffix.empty?
+               "接頭辞を[#{prefix}]、接尾辞を[#{suffix}]にしました。"
+             when prefix_changed && prefix.empty?
+               "接頭辞を消去しました。"
+             when prefix_changed
+               "接頭辞を[#{prefix}]に変更しました。"
+             when suffix_changed && suffix.empty?
+               "接尾辞を消去しました。"
+             when suffix_changed
+               "接尾辞を[#{suffix}]に変更しました。"
+             else
+               "接頭辞および接尾辞の変更は行なわれていません。"
+             end
+      if overflow
+        msg += "文字数オーバーのため、切り詰めています。"
+      end
+      service.twitter.update(message: msg, replyto: message.id)
+    end
+  end
+
   # -----------------------------------
 
   stream_command(:update_name,
@@ -29,25 +71,42 @@ Plugin.create(:stream_command_update_profile) do
 
   # -----------------------------------
 
+  stream_command(:update_prefix,
+                  rate_limit: 3,
+                  rate_limit_reset: 15) do |msg, *args|
+    service = Service.find { |s| msg.receive_to? s.user_obj }
+    
+    if args[0] == 'clear'
+      update_name(service, msg, prefix: '')
+    else
+      update_name(service, msg, prefix: args[0])
+    end
+  end
+
+  # -----------------------------------
+
   stream_command(:update_suffix,
                   rate_limit: 3,
                   rate_limit_reset: 15) do |msg, *args|
     service = Service.find { |s| msg.receive_to? s.user_obj }
-    base_name = UserConfig[:sc_update_profile_base_name]
 
     if args[0] == 'clear'
-      (service.twitter/'account/update_profile').json(name: base_name).next do
-        UserConfig[:sc_update_profile_suffix] = ''
-        service.twitter.update(message: "@#{msg.user.idname} 接尾辞を消去します",
-                               replyto: msg.id)
-      end
+      update_name(service, msg, suffix: '')
     else
-      (service.twitter/'account/update_profile').json(name: base_name + args[0]).next do
-        UserConfig[:sc_update_profile_suffix] = args[0]
-        service.twitter.update(message: "@#{msg.user.idname} 接尾辞を\"#{args[0]}\"に変更します",
-                               replyto: msg.id)
-      end
+      update_name(service, msg, suffix: args[0])
     end
+  end
+
+  # -----------------------------------
+
+  stream_command(:get_prefix,
+                 rate_limit: 3,
+                 rate_limit_reset: 15) do |msg, *args|
+    service = Service.find { |s| msg.receive_to? s.user_obj }
+
+    prefix = UserConfig[:sc_update_profile_prefix]
+    service.twitter.update(message: "@#{msg.user.idname} 現在の接頭辞は[#{prefix}]です。",
+                           replyto: msg.id)
   end
 
   # -----------------------------------
